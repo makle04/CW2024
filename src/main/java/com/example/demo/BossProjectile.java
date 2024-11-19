a2d13a357a9d3a76bd7f5be1445be88a936cfa60
joshua <joshuamak2004@gmail.com>
2024-11-20 00:22:57 +0800

    rename bossfire

--- src/main/java/com/example/demo/BossProjectile.java
+++ src/main/java/com/example/demo/BossProjectile.java
@@ -4,7 +4,7 @@ import javafx.scene.shape.Rectangle;
 
 public class BossProjectile extends Projectile {
 	
-	private static final String IMAGE_NAME = "fireball.png";
+	private static final String IMAGE_NAME = "bossfire.png";
 	private static final int IMAGE_HEIGHT = 65;
 	private static final int HORIZONTAL_VELOCITY = -15;
 	private static final int INITIAL_X_POSITION = 950;

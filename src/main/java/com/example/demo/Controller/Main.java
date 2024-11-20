535902bb7d7057386652c3f5516de3c98aa8ec17
joshua <joshuamak2004@gmail.com>
2024-11-20 18:28:56 +0800

    rename package

--- src/main/java/com/example/demo/Controller/Main.java
+++ src/main/java/com/example/demo/Controller/Main.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.Controller;
 
 import java.lang.reflect.InvocationTargetException;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.demo {
     requires javafx.fxml;
 
 
-    exports com.example.demo.controller;
+    exports com.example.demo.Controller;
     opens com.example.demo.Actors to javafx.fxml;
     opens com.example.demo.Levels to javafx.fxml;
     opens com.example.demo.Views to javafx.fxml;
